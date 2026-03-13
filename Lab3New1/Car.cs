c2354486ab7863b0d0f563426c1cac09c2ec2340
Максим Дмитрачёнок <maksimasdas77@gmail.com>
2026-03-13 09:18:07 +0300

    Добавьте файлы проекта.

---
+++ Lab3New1/Car.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3New1
+{
+    internal class Car
+    {
+        string? name;
+        string? model;
+        int tankvol;
+        int gasvol;
+        int speed;
+        int fuelconsumption;
+        int[] pressureWheel = new int[4];
+
+        public Car():this("рахич", "ракета", 50, 30, 10, 4)
+        {
+        }
+
+        public Car(string? name, string? model, int tankvol, int gasvol, int speed, int fuelconsumption)
+        {
+            Name = name;
+            Model = model;
+            Tankvol = tankvol;
+            Gasvol = gasvol;
+            Speed = speed;
+            Fuelconsumption = fuelconsumption;
+        }
+
+        public Car(string? name, string? model, int tankvol, int gasvol, int speed, int fuelconsumption, params int[] pressureWheel) 
+            : this(name, model, tankvol, gasvol, speed, fuelconsumption)
+        {
+            PressureWheel = pressureWheel;
+        }
+
+        public string? Name { get => name; set => name = value; }
+        public string? Model { get => model; set => model = value; }
+        public int Tankvol
+        {
+            get => tankvol;
+            set
+            {
+                if (value > 0)
+                tankvol = value;
+            }
+        }
+        public int Gasvol
+        {
+            get => gasvol;
+            set
+            {
+                if (value > 0)
+                gasvol = value;
+            }
+        }
+        public int Speed
+        {
+            get => speed;
+            set
+            {
+                if (value >= 0 && value <300)
+                speed = value;
+            }
+        }
+        public int Fuelconsumption
+        {
+            get => fuelconsumption;
+            set
+            {
+                if (value > 0)
+                fuelconsumption = value;
+            }
+        }
+        public int[] PressureWheel
+        {
+            get => pressureWheel;
+            set
+            {
+                for(int i = 0; i < value.Length && i < pressureWheel.Length; i++)
+                {
+                    if(value[i] >= 0 && value[i] <= 5)
+                        pressureWheel[i] = value[i];
+                }
+            }
+        }
+
+        public double Lost()
+        {
+            return (gasvol / fuelconsumption * 100);
+        }
+
+        public void Hole()
+        {
+            for(int i = 0; i < pressureWheel.Length; i++)
+            {
+                if (pressureWheel[i] < 1) Speed = 0;
+            }
+
+        }
+
+        public bool Faster(Car other)
+        {
+            return this.Speed > other.Speed;
+        }
+            public static Car? TheFstest(Car car1, Car car2, Car car3)
+            {
+                if (car1.speed > car2.speed && car1.speed > car3.speed) return car1;
+                if (car2.speed > car3.speed) return car2;
+                else return car3;
+            }
+        
+        public override string? ToString()
+        {
+            return $"{name} {model} {tankvol} {gasvol} {speed} {fuelconsumption} " + $"{pressureWheel[0]}-{pressureWheel[1]}-{pressureWheel[2]}-{pressureWheel[3]}";
+        }
+    }
+}
---
+++ Lab3New1/Program.cs
@@ -0,0 +1,65 @@
+﻿using Lab3New1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car1 = new Car();
+        car1.Name = "Рахич";
+        car1.Model = "Ракета";
+        car1.Tankvol = 50;
+        car1.Gasvol = 40;
+        car1.Speed = 200;
+        car1.Fuelconsumption = 4;
+        car1.PressureWheel = new int[] { 5, 2, 2, 4 };
+        Console.WriteLine(car1.ToString());
+        Console.Write("Еще сможет проехать ");
+        Console.WriteLine(car1.Lost());
+        car1.Hole();
+        if (car1.Speed == 0) Console.WriteLine("Автомобиль остановлен");
+
+        Car car2 = new Car("дракон", "рыгалет", 10, 10, 10, 4, 1, 1, 1, 1);
+        Console.WriteLine(car2.ToString());
+        Console.Write("Еще сможет проехать ");
+        Console.WriteLine(car2.Lost());
+        car2.Hole();
+        if (car2.Speed == 0) Console.WriteLine("Автомобиль остановлен");
+
+        Car car3 = new Car("метла", "конвеер", 100, 40, 90, 10, new int[] { 1, 0, 5, 5 });
+        Console.WriteLine(car3.ToString());
+
+        Console.Write("Еще сможет проехать ");
+        Console.WriteLine(car3.Lost());
+        car3.Hole();
+        if (car3.Speed == 0) Console.WriteLine("Автомобиль остановлен");
+
+        Car car4 = new Car("тачила", "атлас", 200, 40, 90, 10, 2, 2, 2, 5);
+        Console.WriteLine(car4.ToString());
+        Console.Write("Еще сможет проехать ");
+        Console.WriteLine(car4.Lost());
+        car4.Hole();
+        if (car4.Speed == 0) Console.WriteLine("Автомобиль остановлен");
+
+        if (car1.Faster(car2))
+        {
+            Console.WriteLine($"автомобиль {car1.Name} быстрее {car2.Name}");
+        }
+        else
+        { 
+            Console.WriteLine($"автомобиль {car2.Name} быстрее {car1.Name}");
+        }
+        if (car2.Faster(car3))
+        {
+            Console.WriteLine($"автомобиль {car2.Name} быстрее {car3.Name}");
+        }
+        else
+        {
+            Console.WriteLine($"автомобиль {car3.Name} быстрее {car2.Name}");
+        }
+        Car fastest = Car.TheFstest(car1, car2, car3);
+        Console.Write("Самый быстрый автомобиль: " + fastest.Name);
+      
+
+        Console.ReadLine();
+    }
+}
